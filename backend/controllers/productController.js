491d8e77295c0e31a9148410f93cffd5c70added
Lekheshwar <hilekhesh20@gmail.com>
2021-07-12 15:09:07 +0530

    Added Pagination

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,6 +2,7 @@ const Product = require("../models/product");
 
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlerwares/catchAsyncErrors");
+const APIFeatures = require("../utils/apiFeatures");
 
 // Creating new product -----> /api/v1/product/new
 exports.newProduct = catchAsyncErrors(async (req, res, next) => {
@@ -13,14 +14,22 @@ exports.newProduct = catchAsyncErrors(async (req, res, next) => {
   });
 });
 
-// Get all product ----> /api/v1/products
+// Get all products ----> /api/v1/products
 exports.getProducts = catchAsyncErrors(async (req, res, next) => {
-  const products = await Product.find();
+  const RPP = 4;
+  const productCount = await Product.countDocuments();
+  const apiFeatures = new APIFeatures(Product.find(), req.query)
+    .search()
+    .filter()
+    .pagination(RPP);
+
+  const products = await apiFeatures.query;
 
   res.status(200).json({
     success: true,
     count: products.length,
     message: "Products",
+    productCount,
     products,
   });
 });
