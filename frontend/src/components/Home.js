5c01299b34b5bf3360f40dc37f48af44ee65f818
Lekheshwar <hilekhesh20@gmail.com>
2021-07-28 20:00:01 +0530

    Added Loader

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,6 +4,7 @@ import MetaData from "./layout/MetaData";
 import { useDispatch, useSelector } from "react-redux";
 import { getProducts } from "../actions/productActions";
 import Product from "./product/Product";
+import Loader from "./layout/Loader";
 
 export default function Home() {
   const dispatch = useDispatch();
@@ -18,16 +19,22 @@ export default function Home() {
 
   return (
     <>
-      <MetaData title={"Buy Best Product online"} />
-      <h1 id="products_heading">Latest Products</h1>
-      <section id="products" className="container mt-5">
-        <div className="row">
-          {products &&
-            products.map((product) => (
-              <Product key={product._id} product={product} />
-            ))}
-        </div>
-      </section>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <MetaData title={"Buy Best Product online"} />
+          <h1 id="products_heading">Latest Products</h1>
+          <section id="products" className="container mt-5">
+            <div className="row">
+              {products &&
+                products.map((product) => (
+                  <Product key={product._id} product={product} />
+                ))}
+            </div>
+          </section>
+        </>
+      )}
     </>
   );
 }
